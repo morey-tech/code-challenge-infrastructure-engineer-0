008664c586342ab6403f4591e86021f0c6039d5a
Nicholas Morey <nicholas@morey.tech>
2022-01-15 12:32:11 -0500

    feat: add k8s provider

--- providers.tf
+++ providers.tf
@@ -3,3 +3,14 @@ provider "google" {
   project     = var.project_id
   region      = var.region
 }
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/client_config
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.primary.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
+  )
+}
--- versions.tf
+++ versions.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/google"
       version = "4.6.0"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.7.1"
+    }
   }
 
   # lock tf to v1.1.x
